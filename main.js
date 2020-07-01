8cb0e01cae9060de5fc33e02d30e020dadcd9a1a
1025309512@qq.com <1025309512@qq.com>
2020-07-02 01:08:20 +0800

    样式优化 代码略微优化

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var keys = [
+    ['1','2','3','4','5','6','7','8','9','0'],
     ['q','w','e','r','t','y','u','i','o','p'],
     ['a','s','d','f','g','h','j','k','l'],
-    ['z','x','c','v','b','n','m',','],
+    ['z','x','c','v','b','n','m'],
 ]
 
 var hash = {
@@ -14,30 +15,47 @@ var hashInlocalStorage = JSON.parse(localStorage.getItem('data') || 'null')
 if (hashInlocalStorage) {
     hash = hashInlocalStorage
 }
-index = 0
-while (index<keys.length) {
+
+//1.使用while循环创建了3个DIV 每个div中有对应的kbd img button
+//2.对不能或许到的或者没有获取到icon自己给一个icon
+//让icon在输入新网页后进行自动获取icon
+for (let index = 0; index < keys.length; index++) {
     let newDiv = document.createElement("div")
     masker.appendChild(newDiv)   
     let row = keys[index]
-    index1 = 0
-    while (index1<row.length) {
+    newDiv.className = 'line'
+    for (let index = 0; index < row.length; index++) {
         let newKbd = document.createElement("kbd")
-        newKbd.textContent = row[index1]
+        newKbd.textContent = row[index]
+        newKbd.className = row[index] 
         let newButton = document.createElement("button")
         newButton.textContent = 'E'
-        newButton.id = row[index1]
+        newButton.id = row[index]//123qwezxc等
+        let newImg = document.createElement("img")
+        if (hash[row[index]]) {
+            newImg.src = 'http://www.'+ hash[row[index]] + '/favicon.ico'
+        } else {
+            newImg.src = 'https://i.loli.net/2020/07/01/wXlJYzoHAEnIZDu.png'
+        }
+        newImg.onerror = function (xxx) {
+            xxx.src = 'https://i.loli.net/2020/07/01/wXlJYzoHAEnIZDu.png'
+        }
         newButton.onclick = function (keys){
             let box = keys.target.id 
             let newWebsite = prompt('输入网址')
+            let upButtom = keys.target.nextSibling
             hash[box] = newWebsite
+            upButtom.src = 'http://www.' + newWebsite +'/favicon.ico'
+            upButtom.onerror = function (xxx){
+                xxx.src = 'https://i.loli.net/2020/07/01/wXlJYzoHAEnIZDu.png'
+            }
             localStorage.setItem('data',JSON.stringify(hash))
-            console.log(hash)
         }
         newDiv.appendChild(newKbd)
         newKbd.appendChild(newButton)
-        index1 = index1 +1
-    }  
-    index = index+1
+        newKbd.appendChild(newImg)
+        
+    }
 }
 
 document.onkeypress = function (monitorKey) {
@@ -45,5 +63,3 @@ document.onkeypress = function (monitorKey) {
     let website = hash[letter]
     window.open('http://'+website, '_blank')
 }
-
-
